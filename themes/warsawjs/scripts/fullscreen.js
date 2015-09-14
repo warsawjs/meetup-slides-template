066deb5b115fd710eaafe6cd8fb4e8fa3e5212c6
Piotr Kowalski <piecioshka@gmail.com>
2015-09-14 15:17:00 +0200

    Clean code: replace tab by spaces.

--- themes/warsawjs/scripts/fullscreen.js
+++ themes/warsawjs/scripts/fullscreen.js
@@ -1,33 +1,8 @@
-(function () {
+(function (root) {
     'use strict';
 
-    /**
-     * @see: https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
-     */
-    function toggleFullScreen() {
-	if (!document.fullscreenElement &&    // alternative standard method
-	    !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
-	    if (document.documentElement.requestFullscreen) {
-		document.documentElement.requestFullscreen();
-	    } else if (document.documentElement.msRequestFullscreen) {
-		document.documentElement.msRequestFullscreen();
-	    } else if (document.documentElement.mozRequestFullScreen) {
-		document.documentElement.mozRequestFullScreen();
-	    } else if (document.documentElement.webkitRequestFullscreen) {
-		document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-	    }
-	} else {
-	    if (document.exitFullscreen) {
-		document.exitFullscreen();
-	    } else if (document.msExitFullscreen) {
-		document.msExitFullscreen();
-	    } else if (document.mozCancelFullScreen) {
-		document.mozCancelFullScreen();
-	    } else if (document.webkitExitFullscreen) {
-		document.webkitExitFullscreen();
-	    }
-	}
-    }
+    // Alias for `window.document`.
+    var document = root.document;
 
     // Catch reference to button.
     var link = document.querySelector('.badge-left');
@@ -35,9 +10,37 @@
     // Link must be available in DOM.
     if (!link) return;
 
+    /**
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API|MDN}
+     */
+    function toggleFullscreen() {
+        if (!document.fullscreenElement &&    // alternative standard method
+            !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.msRequestFullscreen) {
+                document.documentElement.msRequestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            }
+        }
+    }
+
     // Add listener for 'click'.
     link.addEventListener('click', function (evt) {
-	evt.preventDefault();
-	toggleFullScreen();
+        evt.preventDefault();
+        toggleFullscreen();
     });
-}());
+}(this));
